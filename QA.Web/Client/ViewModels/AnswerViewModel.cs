56f18dd71548340d8e4d36b6c3569985e327f4e3
khaledmousa <khaled.mousa@gmail.com>
2019-09-08 16:23:45 +0100

    some testing with markdown editor
    Add an answer page to test the markdown preview bit .. this should be
    moved to a component to be re-used in posting questions as well

---
+++ QA.Web/Client/ViewModels/AnswerViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Components;
+using QA.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QA.Web.Client.ViewModels
+{
+    public class AnswerViewModel
+    {
+        private readonly HttpClient _httpClient;
+        private readonly NavigationManager _navigationManager;
+        public string QuestionId;
+        public Question Question;
+
+        private Action _refresh;
+        
+        public string AnswerText { get; set; }
+
+        public MarkupString Markdown { get; set; }
+
+        public AnswerViewModel(HttpClient httpClient, NavigationManager navigationManager, string questionId, Action refresh)
+        {
+            _httpClient = httpClient;
+            _navigationManager = navigationManager;
+            QuestionId = questionId;
+            _refresh = refresh;
+        }
+
+        public async Task InitializeAsync()
+        {
+            Question = await _httpClient.GetJsonAsync<Question>($"/api/Post/{QuestionId}");
+        }
+
+        public void OnInputChanged(ChangeEventArgs e)
+        {
+            AnswerText = e.Value.ToString();
+            Markdown = (MarkupString)BuildHtmlFromMarkdown(AnswerText);
+            _refresh?.Invoke();
+        }
+
+        private string BuildHtmlFromMarkdown(string value) => Markdig.Markdown.ToHtml(
+            markdown: value,
+            pipeline: new MarkdownPipelineBuilder().UseAdvancedExtensions().Build()
+        );
+    }
+}
