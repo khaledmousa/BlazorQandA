7a0bc5d6444c72057b0946cedf90ac94b6d3bc02
khaled.mousa@gmail.com <khaled.mousa@gmail.com>
2019-10-19 00:48:40 +0100

    login controller

---
+++ QA.Web/Server/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using QA.Domain.Services;
+
+namespace QA.Web.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAuthenticationService _authenticationService;
+
+        public LoginController(IConfiguration configuration, IAuthenticationService authenticationService)
+        {
+            _configuration = configuration;
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)          
+        {
+            //based on https://chrissainty.com/securing-your-blazor-apps-authentication-with-clientside-blazor-using-webapi-aspnet-core-identity/
+
+            var result = _authenticationService.Login(email, password);
+
+            if (result == null) return BadRequest(null);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Email, email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSecurityKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expiry = DateTime.Now.AddDays(Convert.ToInt32(_configuration["JwtExpiryInDays"]));
+
+            var token = new JwtSecurityToken(
+                _configuration["JwtIssuer"],
+                _configuration["JwtAudience"],
+                claims,
+                expires: expiry,
+                signingCredentials: creds
+            );
+
+            return Ok(new JwtSecurityTokenHandler().WriteToken(token));
+        }
+    }
+}
